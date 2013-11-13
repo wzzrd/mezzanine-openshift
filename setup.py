d3870228c20893481251cfbcc5437e8f5cb346fa
Maxim Burgerhout <maxim@wzzrd.com>
2013-11-13 20:18:07 +0100

    Bump mezzanine, django versions

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='mezzanine-openshift',
-    version='1.2',
+    version='1.2.1',
     description='Mezzanine configured for deployment on OpenShift.',
     author='Isaac Bythewood',
     author_email='isaac@bythewood.me',
     url='http://isaacbythewood.com/',
     install_requires=[
-        'Django==1.5.1',
-        'mezzanine==1.4.7',
+        'Django==1.5.5',
+        'mezzanine==1.4.16',
         'django_compressor==1.3'
     ],
 )
